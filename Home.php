930eb44c8b1c75cdb4d4aeecf47256f48f45b68c
Bruno Viana <brunov25@outlook.com>
2022-11-11 18:50:57 -0300

    11/11 18:50 Criação html e css paginas feed e home (versão mobile)

---
+++ Home.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="css/home.css" media="screen" />
+    <title>GetJobs</title>
+</head>
+<body>
+    <div class=e151_0>
+    <div class="e385_909"></div>
+    <div class=e494_623>
+        <div class=e488_616>
+        <div class="e422_2828"></div><span  class="e151_90">Para Minha Casa</span>
+        </div>
+        <div class=e422_2826>
+        <div class=e151_86><span  class="ei151_86_151_69">Limpeza</span><span  class="ei151_86_151_74"></span>
+            <div class="ei151_86_402_2072"></div>
+        </div>
+        <div class=e151_92><span  class="ei151_92_151_69">Obra</span>
+            <div class="ei151_92_402_2072"></div>
+        </div>
+        <div class=e151_96><span  class="ei151_96_151_69">Arquitetura</span>
+            <div class="ei151_96_402_2072"></div>
+        </div>
+        </div>
+    </div>
+    <div class=e494_624>
+        <div class=e488_615>
+        <div class="e422_2829"></div><span  class="e151_105">Para Minha Empresa</span>
+        </div>
+        <div class=e422_2827>
+        <div class=e151_102><span  class="ei151_102_151_69">Designer</span>
+            <div class="ei151_102_402_2072"></div>
+        </div>
+        <div class=e151_103><span  class="ei151_103_151_69">Desenvolvedor</span>
+            <div class="ei151_103_402_2072"></div>
+        </div>
+        <div class=e151_104><span  class="ei151_104_151_69">Analista</span>
+            <div class="ei151_104_402_2072"></div>
+        </div>
+        </div>
+    </div>
+    <div class=e488_617>
+        <div class=e151_6>
+        <div class="ei151_6_466_1091"></div>
+        <div class="ei151_6_466_1132"></div>
+        <div class="ei151_6_385_1350"></div>
+        <div class="ei151_6_385_1351"></div>
+        </div>
+    </div>
+    <div class=e488_618>
+        <div class="e404_2131"></div><span  class="e258_896">GetJobs</span>
+    </div>
+    </div>
+</body>
+</html>
