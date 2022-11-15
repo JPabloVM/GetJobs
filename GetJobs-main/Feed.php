8b432ec876cfafb61c261635b141487be3b61798
Douglas Alves <92764334+DougA190@users.noreply.github.com>
2022-11-14 23:30:52 -0300

    Testando perfil

---
+++ GetJobs-main/Feed.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="css/feed.css" media="screen" />
+    <title>GetJobs</title>
+</head>
+<body>
+    <div class=e144_2663>
+        <div class="e385_910"></div>
+        <div class=e150_991>
+            <div class=e150_493>
+            <div class="ei150_493_147_490"></div><span  class="ei150_493_147_491">Julio Santos</span><span  class="ei150_493_147_492">Illustrator - UX - Photoshop </span>
+            <div class="ei150_493_147_494"></div>
+            </div>
+            <div class=e150_534>
+            <div class="ei150_534_147_490"></div><span  class="ei150_534_147_491">Gabriela Novais</span><span  class="ei150_534_147_493"></span><span  class="ei150_534_147_492">Designer - UX - UI</span>
+            <div class="ei150_534_147_494"></div>
+            </div>
+            <div class=e150_540>
+            <div class="ei150_540_147_490"></div><span  class="ei150_540_147_491">João Passos</span><span  class="ei150_540_147_493"> </span><span  class="ei150_540_147_492">Ux - photoshop</span>
+            <div class="ei150_540_147_494"></div>
+            </div>
+            <div class=e150_546>
+            <div class="ei150_546_147_490"></div><span  class="ei150_546_147_491">Julio Rorigues</span><span  class="ei150_546_147_493"></span><span  class="ei150_546_147_492">INdesign - word</span>
+            <div class="ei150_546_147_494"></div>
+            </div>
+        </div>
+        <div class=e150_668>
+            <div class="ei150_668_466_1091"></div>
+            <div class="ei150_668_466_1132"></div>
+            <div class="ei150_668_385_1350"></div>
+            <div class="ei150_668_385_1351"></div>
+        </div><span  class="e258_897">Designer</span>
+        <div class="e307_909"></div>
+    </div>
+</body>
+</html>
