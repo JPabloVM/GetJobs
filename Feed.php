f29831d5cae24608f57b9f265deadb8a4bc8b381
Bruno Viana <brunov25@outlook.com>
2022-11-15 17:43:20 -0300

    15/11 17:42 Telas de Feed e Home Recriadas, conectando ao banco mySql

--- Feed.php
+++ Feed.php
@@ -1,40 +1,63 @@
+<?php
+    $c =  $_GET['cargo'];
+    include("conexao.php");
+    $sql_code = "SELECT * FROM Usuario as U INNER JOIN Habilidades as H on U.id_usuario = H.id_usuario where H.cargo = '$c'";
+    $sql_query = $mysqli->query($sql_code) or die($mysqli->error);
+?>
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" type="text/css" href="css/feed.css" media="screen" />
-    <title>GetJobs</title>
-</head>
-<body>
-    <div class=e144_2663>
-        <div class="e385_910"></div>
-        <div class=e150_991>
-            <div class=e150_493>
-            <div class="ei150_493_147_490"></div><span  class="ei150_493_147_491">Julio Santos</span><span  class="ei150_493_147_492">Illustrator - UX - Photoshop </span>
-            <div class="ei150_493_147_494"></div>
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+        <link rel="stylesheet" href="css/estilos.css">
+        <title>GetJobs</title>
+    </head>
+    <main>
+        <body>
+            <!--Logo e topo da Pagina-->
+            <nav>
+                <div class="nav-wrapper blue darken-3">
+                    <a href="#" class="brand-logo center">GetJobs</a>
+                </div>
+            </nav>
+            <!--fim do Logo e topo da Pagina-->
+            <div class="container">
+                <?php if($sql_query->num_rows > 0) : ?>
+                    <?php while($usuario = $sql_query->fetch_assoc()) :?>
+                        <ul class="collection">
+                            <li class="collection-item avatar">
+                                <img src="<?= $usuario['ftPerfil']?>" alt="usuario" class="circle">
+                                <span class="title"><?= $usuario['nmUsuario'] ?></span>
+                                <p>
+                                    <?= $usuario['descricao']?>
+                                </p>
+                                <a href="Perfil.html?id=<?= $usuario['id_usuario']?>" class="secundary-content"><i class="arrow_forward">perfil</i></a>
+                            </li>
+                        </ul>
+                    <?php endwhile ?>
+                <?php endif ?>
             </div>
-            <div class=e150_534>
-            <div class="ei150_534_147_490"></div><span  class="ei150_534_147_491">Gabriela Novais</span><span  class="ei150_534_147_493"></span><span  class="ei150_534_147_492">Designer - UX - UI</span>
-            <div class="ei150_534_147_494"></div>
-            </div>
-            <div class=e150_540>
-            <div class="ei150_540_147_490"></div><span  class="ei150_540_147_491">João Passos</span><span  class="ei150_540_147_493"> </span><span  class="ei150_540_147_492">Ux - photoshop</span>
-            <div class="ei150_540_147_494"></div>
-            </div>
-            <div class=e150_546>
-            <div class="ei150_546_147_490"></div><span  class="ei150_546_147_491">Julio Rorigues</span><span  class="ei150_546_147_493"></span><span  class="ei150_546_147_492">INdesign - word</span>
-            <div class="ei150_546_147_494"></div>
+
+        </body>
+    </main>
+
+
+    <footer class="page-footer blue-grey lighten-4">
+        <div class="container">
+            <div class="row">
+                <div class="col s6 blue darken-3">
+                    <a href="Home.php" class="rigth-align">
+                        <img src="img/home_fill0_wght400_grad0_opsz48_1.png" alt="menu">
+                    </a>
+                </div>
+                <div class="col s6 blue-grey lighten-2">
+                    <a href="#" class="center-align">
+                        <img src="img/person_fill0_wght400_grad0_opsz48_1.png" alt="perfil">
+                    </a>
+                </div>
             </div>
         </div>
-        <div class=e150_668>
-            <div class="ei150_668_466_1091"></div>
-            <div class="ei150_668_466_1132"></div>
-            <div class="ei150_668_385_1350"></div>
-            <div class="ei150_668_385_1351"></div>
-        </div><span  class="e258_897">Designer</span>
-        <div class="e307_909"></div>
-    </div>
-</body>
+    </footer>
 </html>
