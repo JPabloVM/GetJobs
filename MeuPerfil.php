bd02d6903f2e7beccab6c8a39b71140426d0a5a1
Douglas <douglas.alves.angelo@hotmail.com>
2022-11-16 14:13:43 -0300

    commit

--- MeuPerfil.php
+++ MeuPerfil.php
@@ -12,40 +12,49 @@ $usuarios = $bd->query($sql);
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/meuPerfil.css">
     <title>Meu Perfil</title>
-    <!-- Compiled and minified CSS -->
-
 </head>
 
 <body>
-    </div>
-    </nav>
+    <div class="container">
+        <div class="hamburguer">
+            <div class="line" id="line1"></div>
+            <div class="line" id="line2"></div>
+            <div class="line" id="line3"></div>
+            <span>Fechar</span>
+        </div>
 
+        <?php while ($usuario = $usuarios->fetchArray()) : ?>
 
-    <?php while ($usuario = $usuarios->fetchArray()) : ?>
-        <header>
-            <div class="img-wrapper">
-                <img src="img/perfil/bg.jpg" alt="foto de capa">
-            </div>
-            <div id="nav-mobile" class="solicitar-job">
-                <li><a class="waves-effect waves-light btn" href="#">Editar Perfil</a></li>
-            </div>
+            <header>
+                <div class="img-wrapper">
+                    <img src="img/perfil/bg.jpg" alt="foto de capa">
+                </div>
+                <div class="editar-perfil">
+                    <li><a class="waves-effect waves-light btn" href="EditarPerfil.html">Editar Perfil</a></li>
+                </div>
 
-            <div class="container">
                 <div class="perfil">
                     <img src=<?= $usuario["ftPerfil"] ?> alt="foto de perfil">
                     <h1><?= $usuario["nmUsuario"] ?></h1>
-
+                    <h2>Cargo</h2>
                 </div>
-        </header>
-        <div class="descricao">
+            </header>
+            <div class="descricao">
+            <h1>Sobre mim</h1>
+                <textarea disabled name="descricao"><?= $usuario["descricao"] ?></textarea>
 
-            <textarea disabled name="descricao"><?= $usuario["descricao"] ?></textarea>
+            </div>
+        <?php endwhile ?>
+        <aside class="sidebar">
+            <nav>
+                <ul class="menu">
+                    <li class="menu-item"><a href="#" class="menu-link">Home</a></li>
+                    <li class="menu-item"><a href="#" class="menu-link">Feed</a></li>
+                    <li class="menu-item"><a href="#" class="menu-link">Meu Perfil</a></li>
+                </ul>
+            </nav>
+        </aside>
 
-        </div>
-    <?php endwhile ?>
-    <div class="solicitar-job">
-        <button type="submit">Solicitar Job</button>
-    </div>
 
     </div>
 </body>
