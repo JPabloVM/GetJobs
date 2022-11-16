ad12fe311bbdee9e9b82749fbb33cc4d59f47b39
JPabloVM <joaopablo778@gmail.com>
2022-11-16 12:49:15 -0300

    :update:Comentarios no projeto e adição do banco de dados ao projeto

--- Perfil.php
+++ Perfil.php
@@ -21,6 +21,7 @@ $sql_query = $mysqli->query($sql_code) or die($mysqli->error);
     <link rel="stylesheet" href="css/perfil.css">
     <?php while ($usuario = $sql_query->fetch_assoc()): ?>
     <title>
+        <!--Defino que o titulo da página será o nome do usuário-->
         <?= $usuario['nmUsuario'] ?>
     </title>
 </head>
@@ -28,29 +29,37 @@ $sql_query = $mysqli->query($sql_code) or die($mysqli->error);
 <body>
     <div class="container">
 
+        <!--Inicio do header-->
+        <header>
+            <div class="img-wrapper">
+                <img src="img/perfil/bg.jpg" alt="foto de capa">
+            </div>
+            <div class="perfil">
+                <!--Pego a foto de perfil diretamente do banco usuário-->
+                <img src=<?= $usuario["ftPerfil"] ?> alt="foto de perfil">
+                <h1>
+                    <!--Pego o nome diretamente do banco usuário-->
+                    <?= $usuario['nmUsuario'] ?>
+                </h1>
+                <h2>
+                    <!--Pego o cargo diretamente do banco usuário-->
+                    <?= $usuario['cargo'] ?>
+                </h2>
+            </div>
+        </header>
+        <!--Fim do header-->
+
+        <!--Inicio do menu hamburguer-->
         <div class="hamburguer">
             <div class="line" id="line1"></div>
             <div class="line" id="line2"></div>
             <div class="line" id="line3"></div>
             <span>Fechar</span>
         </div>
+        <!--Fim do menu hamburguer-->
 
-        <header>
-            <div class="img-wrapper">
-                <img src="img/perfil/bg.jpg" alt="foto de capa">
-            </div>
-            <div class="perfil">
-                <img src=<?= $usuario["ftPerfil"] ?> alt="foto de perfil">
-                <h1>
-                    <?= $usuario['nmUsuario'] ?>
-                </h1>
-                <h2>
-                    <?= $usuario['cargo'] ?>
-                </h2>
-            </div>
-        </header>
+        <!--Inicio do sidebar do projeto, onde incluo os links que direcionam o usuário para outras páginas do projeto-->
         <aside class="sidebar">
-
             <nav>
                 <ul class="menu">
                     <li class="menu-item"><a href="Home.php" class="menu-link">Home</a></li>
@@ -58,17 +67,17 @@ $sql_query = $mysqli->query($sql_code) or die($mysqli->error);
                 </ul>
             </nav>
         </aside>
+        <!--Fim do sidebar-->
+
         <!-- Inicio da descricao-->
         <div class="descricao">
-
+            <!--Pego a descricao diretamente do banco usuário-->
             <textarea disabled name="descricao">
             <?= $usuario['descricao'] ?>
-
             </textarea>
-
-
         </div>
         <!-- Fim da descricao-->
+
         <!-- Inicio do botão para solicitar-job-->
         <div class="solicitar-job">
             <button type="submit">Solicitar Job</button>
@@ -78,6 +87,7 @@ $sql_query = $mysqli->query($sql_code) or die($mysqli->error);
 
     </div>
 </body>
+<!--Chamo o script da pasta js para executar o menu hamburguer-->
 <script language="javascript" type="text/javascript" src="js/script.js"></script>
 
 </html>
