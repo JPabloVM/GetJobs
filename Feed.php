f31e16fc2959a2183b41ebbd4f042ce0cb3de6dc
Bruno Viana <brunov25@outlook.com>
2022-11-21 11:42:44 -0300

    21/11 11:42 Ajustes nos menus 'hamburgers'

--- Feed.php
+++ Feed.php
@@ -6,94 +6,73 @@ $sql_query = $mysqli->query($sql_code) or die($mysqli->error);
 ?>
 <!DOCTYPE html>
 <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="apple-touch-icon" sizes="180x180" href="favicon/apple-touch-icon.png">
+        <link rel="icon" type="image/png" sizes="32x32" href="favicon/favicon-32x32.png">
+        <link rel="icon" type="image/png" sizes="16x16" href="favicon/favicon-16x16.png">
+        <link rel="manifest" href="favicon/site.webmanifest">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+        <link rel="stylesheet" href="css/estilos.css">
+        <link rel="stylesheet" href="css/menu.css">
+        <title>GetJobs</title>
+    </head>
+    <main>
+        <body>
+            <div class="container">
+                <!--Inicio do menu hamburguer-->
+                <div class="hamburguer">
+                    <div class="line" id="line1"></div>
+                    <div class="line" id="line2"></div>
+                    <div class="line" id="line3"></div>
+                    <span>Fechar</span>
+                </div>
+                <!--Fim do menu hamburguer-->
 
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="apple-touch-icon" sizes="180x180" href="favicon/apple-touch-icon.png">
-    <link rel="icon" type="image/png" sizes="32x32" href="favicon/favicon-32x32.png">
-    <link rel="icon" type="image/png" sizes="16x16" href="favicon/favicon-16x16.png">
-    <link rel="manifest" href="favicon/site.webmanifest">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
-    <link rel="stylesheet" href="css/estilos.css">
-    <link rel="stylesheet" href="css/menu.css">
-    <title>GetJobs</title>
-</head>
-<main>
-
-    <body>
-
-        <div class="container">
-            <!--Inicio do menu hamburguer-->
-            <div class="hamburguer">
-                <div class="line" id="line1"></div>
-                <div class="line" id="line2"></div>
-                <div class="line" id="line3"></div>
-                <span>Fechar</span>
+                <!--Inicio do sidebar do projeto, onde incluo os links que direcionam o usuário para outras páginas do projeto-->
+                <aside class="sidebar">
+                    <nav class="blue darken-3">
+                        <ul class="menu">
+                            <li class="menu-item"><a href="Home.php" class="menu-link">Home</a></li>
+                            <li class="menu-item"><a href="MeuPerfil.php" class="menu-link">Meu Perfil</a></li>
+                        </ul>
+                    </nav>
+                </aside>
+                <!--Fim do sidebar-->
             </div>
-            <!--Fim do menu hamburguer-->
+            <!--Logo e topo da Pagina-->
+            <nav>
+                <div class="nav-wrapper blue darken-3">
+                    <a href="#" class="brand-logo center">GetJobs</a>
+                </div>
+            </nav>
+            <!--fim do Logo e topo da Pagina-->
+            <div class="container">
 
-            <!--Inicio do sidebar do projeto, onde incluo os links que direcionam o usuário para outras páginas do projeto-->
-            <aside class="sidebar">
-                <nav>
-                    <ul class="menu">
-                        <li class="menu-item"><a href="Home.php" class="menu-link">Home</a></li>
-                        <li class="menu-item"><a href="MeuPerfil.php" class="menu-link">Meu Perfil</a></li>
-                    </ul>
-                </nav>
-            </aside>
-            <!--Fim do sidebar-->
-        </div>
-        <!--Logo e topo da Pagina-->
-        <nav>
-            <div class="nav-wrapper blue darken-3">
-                <a href="#" class="brand-logo center">GetJobs</a>
+                <?php if ($sql_query->num_rows > 0): ?>
+                <?php while ($usuario = $sql_query->fetch_assoc()): ?>
+                <ul class="collection">
+                    <li class="collection-item avatar">
+                        <img src="<?= $usuario['ftPerfil'] ?>" alt="usuario" class="circle">
+                        <span class="title">
+                            <?= $usuario['nmUsuario'] ?>
+                        </span>
+                        <p>
+                            <?= $usuario['descricao'] ?>
+                        </p>
+                        <a href="Perfil.php?id=<?= $usuario['id_usuario'] ?>" class="secundary-content"><i
+                                class="arrow_forward">perfil</i></a>
+                    </li>
+                </ul>
+                <?php endwhile ?>
+                <?php endif ?>
             </div>
-        </nav>
-        <!--fim do Logo e topo da Pagina-->
-        <div class="container">
 
-            <?php if ($sql_query->num_rows > 0): ?>
-            <?php while ($usuario = $sql_query->fetch_assoc()): ?>
-            <ul class="collection">
-                <li class="collection-item avatar">
-                    <img src="<?= $usuario['ftPerfil'] ?>" alt="usuario" class="circle">
-                    <span class="title">
-                        <?= $usuario['nmUsuario'] ?>
-                    </span>
-                    <p>
-                        <?= $usuario['descricao'] ?>
-                    </p>
-                    <a href="Perfil.php?id=<?= $usuario['id_usuario'] ?>" class="secundary-content"><i
-                            class="arrow_forward">perfil</i></a>
-                </li>
-            </ul>
-            <?php endwhile ?>
-            <?php endif ?>
-        </div>
-
-    </body>
-</main>
-
-
-<footer class="page-footer blue-grey lighten-4">
-    <div class="container">
-
-        <div class="row">
-            <div class="col s6 blue darken-3">
-                <a href="Home.php" class="rigth-align">
-                    <img src="img/home_fill0_wght400_grad0_opsz48_1.png" alt="menu">
-                </a>
-            </div>
-            <div class="col s6 blue-grey lighten-2">
-                <a href="MeuPerfil.php" class="center-align">
-                    <img src="img/person_fill0_wght400_grad0_opsz48_1.png" alt="perfil">
-                </a>
-            </div>
-        </div>
-    </div>
-    <script language="javascript" type="text/javascript" src="js/script.js" defer></script>
-</footer>
-
+        </body>
+    </main>
+    <footer class="page-footer blue-grey lighten-4">
+        <script language="javascript" type="text/javascript" src="js/script.js" defer></script>
+    </footer>
 </html>
--- Home.php
+++ Home.php
@@ -1,76 +1,96 @@
 <!DOCTYPE html>
 <html lang="pt-br">
-
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="apple-touch-icon" sizes="180x180" href="favicon/apple-touch-icon.png">
-    <link rel="icon" type="image/png" sizes="32x32" href="favicon/favicon-32x32.png">
-    <link rel="icon" type="image/png" sizes="16x16" href="favicon/favicon-16x16.png">
-    <link rel="manifest" href="favicon/site.webmanifest">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
-    <link rel="stylesheet" href="css/estilos.css">
-    <title>GetJobs</title>
-</head>
-<main>
-
-    <body>
-        <nav>
-            <div class="nav-wrapper blue darken-3">
-                <a href="#" class="brand-logo center">GetJobs</a>
-            </div>
-        </nav>
-        <div class="container">
-            <div class="blue lighten-4">
-                <h3 class="center-align black-text">Para sua Casa</h1>
-                    <div class="row">
-                        <!--Limpeza-->
-                        <div class="col s4">
-                            <div class="card">
-                                <div class="card-image">
-                                    <a href="Feed.php?cargo=Limpeza" class="black-text" name="Limpeza">
-                                        <img src="img/rectangle_31.png" alt="Limpeza" />
-                                    </a>
-                                </div>
-                                <div class="card-action">
-                                    <a href="Feed.php?cargo=Limpeza" class="black-text" name="Limpeza">Limpeza</a>
-                                </div>
-                            </div>
-                        </div>
-                        <!--Fim de Limpeza-->
-                        <!--Obra-->
-                        <div class="col s4">
-                            <div class="card">
-                                <div class="card-image">
-                                    <a href="Feed.php?cargo=Obra" class="black-text" name="Obra">
-                                        <img src="img/rectangle_32.png" alt="Obra" />
-                                    </a>
-                                </div>
-                                <div class="card-action">
-                                    <a href="Feed.php?cargo=Obra" class="black-text" name="Obra">Obra</a>
-                                </div>
-                            </div>
-                        </div>
-                        <!--Fim de Obra-->
-                        <!--Arquitetura-->
-                        <div class="col s4">
-                            <div class="card">
-                                <div class="card-image">
-                                    <a href="Feed.php?cargo=Arquitetura" class="black-text">
-                                        <img src="img/rectangle_33.jpg" alt="Arquitetura" />
-                                    </a>
-                                </div>
-                                <div class="card-action">
-                                    <a href="Feed.php?cargo=Arquitetura" class="black-text">Arquitetura</a>
-                                </div>
-                            </div>
-                        </div>
-                        <!--Fim de Arquitetura-->
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="apple-touch-icon" sizes="180x180" href="favicon/apple-touch-icon.png">
+        <link rel="icon" type="image/png" sizes="32x32" href="favicon/favicon-32x32.png">
+        <link rel="icon" type="image/png" sizes="16x16" href="favicon/favicon-16x16.png">
+        <link rel="manifest" href="favicon/site.webmanifest">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+        <link rel="stylesheet" href="css/estilos.css">
+        <link rel="stylesheet" href="css/menu.css">
+        <title>GetJobs</title>
+    </head>
+    <main>
+        <body>
+            <div class="container">
+                    <!--Inicio do menu hamburguer-->
+                    <div class="hamburguer">
+                        <div class="line" id="line1"></div>
+                        <div class="line" id="line2"></div>
+                        <div class="line" id="line3"></div>
+                        <span>Fechar</span>
                     </div>
+                    <!--Fim do menu hamburguer-->
+
+                    <!--Inicio do sidebar do projeto, onde incluo os links que direcionam o usuário para outras páginas do projeto-->
+                    <aside class="sidebar">
+                        <nav class="blue darken-3">
+                            <ul class="menu">
+                                <li class="menu-item"><a href="Home.php" class="menu-link">Home</a></li>
+                                <li class="menu-item"><a href="MeuPerfil.php" class="menu-link">Meu Perfil</a></li>
+                            </ul>
+                        </nav>
+                    </aside>
+                <!--Fim do sidebar-->
             </div>
-            <div class="blue lighten-4">
-                <h3 class="center-align black-text">Para sua Empresa</h1>
+            <nav>
+                <div class="nav-wrapper blue darken-3">
+                    <a href="#" class="brand-logo center">GetJobs</a>
+                </div>
+            </nav>
+            <div class="container">
+                <div class="blue lighten-4">
+                    <h3 class="center-align black-text">Para sua Casa</h1>
+                        <div class="row">
+                            <!--Limpeza-->
+                            <div class="col s4">
+                                <div class="card">
+                                    <div class="card-image">
+                                        <a href="Feed.php?cargo=Limpeza" class="black-text" name="Limpeza">
+                                            <img src="img/rectangle_31.png" alt="Limpeza" />
+                                        </a>
+                                    </div>
+                                    <div class="card-action">
+                                        <a href="Feed.php?cargo=Limpeza" class="black-text" name="Limpeza">Limpeza</a>
+                                    </div>
+                                </div>
+                            </div>
+                            <!--Fim de Limpeza-->
+                            <!--Obra-->
+                            <div class="col s4">
+                                <div class="card">
+                                    <div class="card-image">
+                                        <a href="Feed.php?cargo=Obra" class="black-text" name="Obra">
+                                            <img src="img/rectangle_32.png" alt="Obra" />
+                                        </a>
+                                    </div>
+                                    <div class="card-action">
+                                        <a href="Feed.php?cargo=Obra" class="black-text" name="Obra">Obra</a>
+                                    </div>
+                                </div>
+                            </div>
+                            <!--Fim de Obra-->
+                            <!--Arquitetura-->
+                            <div class="col s4">
+                                <div class="card">
+                                    <div class="card-image">
+                                        <a href="Feed.php?cargo=Arquitetura" class="black-text">
+                                            <img src="img/rectangle_33.jpg" alt="Arquitetura" />
+                                        </a>
+                                    </div>
+                                    <div class="card-action">
+                                        <a href="Feed.php?cargo=Arquitetura" class="black-text">Arquitetura</a>
+                                    </div>
+                                </div>
+                            </div>
+                            <!--Fim de Arquitetura-->
+                        </div>
+                </div>
+                <div class="blue lighten-4">
+                    <h3 class="center-align black-text">Para sua Empresa</h3>
                     <div class="row">
                         <!--Designer-->
                         <div class="col s4">
@@ -115,25 +135,11 @@
                         </div>
                         <!--Fim de Analista-->
                     </div>
+                </div>
             </div>
-        </div>
-    </body>
-</main>
-<footer class="page-footer blue-grey lighten-4">
-    <div class="container">
-        <div class="row">
-            <div class="col s6 blue darken-3">
-                <a href="Home.php" class="rigth-align">
-                    <img src="img/home_fill0_wght400_grad0_opsz48_1.png" alt="menu">
-                </a>
-            </div>
-            <div class="col s6 blue-grey lighten-2">
-                <a href="MeuPerfil.php" class="center-align">
-                    <img src="img/person_fill0_wght400_grad0_opsz48_1.png" alt="perfil">
-                </a>
-            </div>
-        </div>
-    </div>
-</footer>
-
+        </body>
+    </main>
+    <footer class="page-footer blue-grey lighten-4">
+        <script language="javascript" type="text/javascript" src="js/script.js" defer></script>
+    </footer>
 </html>
