6fd124ebf3ad45c35e2b26ea4a0f2341057ce1e0
IgorFOliveira97 <igor.io756@gmail.com>
2022-11-30 14:54:52 -0300

    Ajsutes Editar Perfil

--- EditarPerfil.php
+++ EditarPerfil.php
@@ -29,15 +29,12 @@ $con = $mysqli->query($consulta) or die($mysqli->error);
     <div id=main-container>
         <h1> Seu Perfil</h1>
         <a href="MeuPerfil.php">
-            <h3>Sair</h3>
+                <p><img class="sair" src="img/Sair.png" alt="Sair do Perfil" id="sair" width="50" height="50"></p>
+            </a>
             
         </a> 
         <?php while ($dados = $con->fetch_array()) { ?>
-            <a class="btn btn-sm btn-primary" href='Editar.php?id=$dados[id_usuario]'>
-        <svg xmlns='http://www.w3.org/2000/svg' width='25' height='25' fill='currentColor' class='bi bi-pencil' viewBox='0 0 16 16'>
-  <path d='M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168l10-10zM11.207 2.5 13.5 4.793 14.793 3.5 12.5 1.207 11.207 2.5zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293l6.5-6.5zm-9.761 5.175-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325z'/>
-  
-</svg>
+            
 
         </a>
             <form id="register-form" action="Editar.php" method="POST" >
@@ -122,12 +119,11 @@ $con = $mysqli->query($consulta) or die($mysqli->error);
                     <label for="senha">Senha</label>
                     <input type="password" name="senha" id="senha" value="<?php echo $dados["senha"] ?>">
                 </div>
-                <!--Botão deletar-->
+                <!--Botão editar-->
                 <div class="full-box">
-                    <a href="Deletar.php">
-                        <input class="deletar" type="submit" id="btn-submit" value="Deletar Conta" name="Deletar">
-                    </a>    
-                </div>
+                    <a href="Editar.php">
+                        <input class="editar" type="submit" id="btn-submit" value="Editar Perfil" name="Editar">
+                    </a>
             </form>
             <?php } ?>
             
