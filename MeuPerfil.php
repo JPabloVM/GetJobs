22102a565935163b3f4b5a2a9ad54a131ace5d82
JPabloVM <joaopablo778@gmail.com>
2023-03-12 00:59:57 -0300

    Teste

--- MeuPerfil.php
+++ MeuPerfil.php
@@ -1,47 +1,50 @@
-<!DOCTYPE html>
-<html lang="pt-br">
+<?php ?>
+
+<?php
+$bd = new SQLite3("PI.sql");
+$sql = "SELECT * FROM Usuario";
+$usuarios = $bd->query($sql);
+?>
 
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/perfil.css">
-    <title>Perfil</title>
-    <!-- Compiled and minified CSS -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+    <link rel="stylesheet" href="css/meuPerfil.css">
+    <title>Meu Perfil</title>
 
-    <!-- Compiled and minified JavaScript -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
 </head>
 
 <body>
-  </div>
-</nav>
-    <div class="container">
+    </div>
+    </nav>
 
+
+    <?php while ($usuario = $usuarios->fetchArray()) : ?>
         <header>
-            <div id="nav-mobile" class="perfil">
-                <li><a class="waves-effect waves-light btn" href="#">Editar Perfil</a></li>
-              </div>
             <div class="img-wrapper">
                 <img src="img/perfil/bg.jpg" alt="foto de capa">
-                
             </div>
-            <div class="perfil">
-                <img src="img/perfil/perfil.png" alt="foto de perfil">
-                <h1>Nome</h1>
-                
+            <div id="nav-mobile" class="solicitar-job">
+                <li><a class="waves-effect waves-light btn" href="#">Editar Perfil</a></li>
             </div>
+
+            
+                <div class="perfil">
+                    <img src=<?= $usuario["ftPerfil"] ?> alt="foto de perfil">
+                    <h1><?= $usuario["nmUsuario"] ?></h1>
+
+                </div>
         </header>
         <div class="descricao">
 
-            <textarea disabled
-                name="descricao">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa vero vel totam accusamus nesciunt possimus odit incidunt laboriosam, consectetur ea minus recusandae. Error consectetur incidunt ipsam quae exercitationem cupiditate dolorum.lore Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet fugiat, vero cupiditate ducimus magni saepe accusamus reiciendis mollitia iure deleniti quidem aliquid in repudiandae exercitationem quas facilis totam? Inventore, mollitia! Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem, blanditiis eligendi culpa officiis, corrupti aliquid asperiores sint explicabo error doloribus commodi in tenetur iste atque eos nihil fuga perferendis esse. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iusto, obcaecati nostrum! Quos voluptas vel harum dolor laudantium, debitis sed. Repellendus tempore dolores quis veniam natus adipisci nisi expedita, animi eligendi. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laborum, aut cum in facere quod explicabo eos recusandae qui voluptatem optio perferendis, beatae consectetur atque sint dignissimos, delectus voluptatum error consequatur. Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt error cupiditate blanditiis, quo quia harum quod quis itaque corporis ullam at id incidunt accusamus? Ullam tenetur odit error voluptas nulla? Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eius, sed inventore id eos esse aspernatur. Ipsam, placeat exercitationem! Facere, nemo tenetur minus totam architecto dolores maxime ipsa a quis illo. Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus beatae a distinctio sit tenetur fugit reiciendis, minima sed laborum sequi ab labore quasi, eum explicabo corporis optio libero sunt. Ut! Lorem ipsum dolor, sit amet consectetur adipisicing elit. Molestias molestiae itaque facere officia explicabo totam, porro at velit laudantium ea veritatis soluta, enim accusamus! Dicta voluptate nostrum ipsum inventore nulla. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quisquam quae fugiat veniam debitis harum sint, quasi ut ex quis consequatur impedit sit. Quae perspiciatis iste nam molestias velit debitis odit.</textarea>
+            <textarea disabled name="descricao"><?= $usuario["descricao"] ?></textarea>
 
         </div>
-        <div class="solicitar-job">
-            <button type="submit">Solicitar Job</button>
-        </div>
+    <?php endwhile ?>
+    <div class="solicitar-job">
+        <button type="submit">Solicitar Job</button>
+    </div>
 
     </div>
 </body>
